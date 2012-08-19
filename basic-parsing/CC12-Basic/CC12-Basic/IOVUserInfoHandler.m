d4e1ba8b69c8314d4b00bd07442520f8b9ea66fe
Travis Fischer <travis@iovortex.com>
2012-08-19 12:50:32 -0500

    Hydrate JSOn into IOVUser objects.

--- basic-parsing/CC12-Basic/CC12-Basic/IOVUserInfoHandler.m
+++ basic-parsing/CC12-Basic/CC12-Basic/IOVUserInfoHandler.m
@@ -7,6 +7,13 @@
 //
 
 #import "IOVUserInfoHandler.h"
+#import "IOVAppDelegate.h"
+#import "IOVUser.h"
+
+@interface IOVUserInfoHandler ()
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+@end
 
 @implementation IOVUserInfoHandler
 
@@ -14,10 +21,32 @@
     return [[IOVUserInfoHandler alloc] init];
 }
 
+-(id)init {
+    self = [super init];
+    if (self) {
+        IOVAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+        
+        _managedObjectContext = [appDelegate newManagedObjectContext];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleDidSaveNotification:) name:NSManagedObjectContextDidSaveNotification object:nil];
+    }
+    return self;
+}
+
+-(void)handleDidSaveNotification:(NSNotification*)notification {
+    [self.managedObjectContext mergeChangesFromContextDidSaveNotification:notification];
+}
+
 -(void)handleJSON:(id)jsonObject {
     
     NSLog(@"JSON: %@", jsonObject);
+    IOVUser *user;
     
+    for (NSDictionary *dict in jsonObject) {
+        user = [IOVUser objectFromJSON:dict withParameters:nil andContext:self.managedObjectContext];
+        [self.managedObjectContext save:nil];
+    }
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationUpdatedUser object:self userInfo:@{ kUserInfoUserName : user.userName }];
 }
 
 @end
